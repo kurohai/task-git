3f3b5e59f55d46534d07e41194a37f7fcf811fb7
Evan Burt <kurohai@gmail.com>
2019-08-15 16:18:43 -0500

    added task-push, task-pull, and task-sync functions

--- task-alias.sh
+++ task-alias.sh
@@ -7,3 +7,28 @@ then
 fi
 
 alias task='${PROJECT_HOME}/task-git/task-git.sh'
+alias task-git-push='${PROJECT_HOME}/task-git/task-git.sh --task-git-push'
+
+
+function task-pull() {
+    echo "performing git pull"
+    cd ${PROJECT_HOME}/task-config/
+    git checkout develop
+    git pull origin develop
+    echo "git pull complete"
+}
+
+function task-push() {
+    echo "performing git push"
+    cd ${PROJECT_HOME}/task-config/
+    git checkout develop
+    git push origin develop
+    echo "git push complete"
+}
+
+function task-sync() {
+    echo "starting task sync"
+    tash-pull
+    task-push
+    echo "task sync complete"
+}
