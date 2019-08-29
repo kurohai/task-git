931f31cc5dbd043655ac28293b3128d4e41b117c
Evan Burt <kurohai@gmail.com>
2019-08-29 05:09:06 -0500

    added logging and sync lock to task-sync

--- task-alias.sh
+++ task-alias.sh
@@ -9,11 +9,22 @@
 # fi
 
 export TASKDATA=${PROJECT_HOME}/task-config
-export TASKRC=${PROJECT_HOME}/task-config/.taskrc-osx
+export TASKRC=${PROJECT_HOME}/task-config/.taskrc
+# LOG_FILE=${TASKDATA}/log/task-sync-`date +%s`.log
+TASK_SYNC_LOG_FILE=${TASKDATA}/log/task-sync-`date +%Y-%m-%d`.log
+TASK_SYNC_LOCK_FILE=${TASKDATA}/.sync_lock
+
+if [ ! -d "${TASKDATA}/log" ];
+then
+    mkdir -p "${TASKDATA}/log"
+fi
 
 alias task='${PROJECT_HOME}/task-git/task-git.sh'
 alias task-git-push='${PROJECT_HOME}/task-git/task-git.sh --task-git-push'
 
+function t() {
+    ${PROJECT_HOME}/task-git/task-git.sh $@
+}
 
 
 function task-pull() {
@@ -35,10 +46,16 @@ function task-push() {
 }
 
 function task-sync() {
-    echo "starting task sync"
-    task-pull
-    task-push
-    echo "task sync complete"
+    if [ ! -f "${TASK_SYNC_LOCK_FILE}" ];
+    then
+        touch "${TASK_SYNC_LOCK_FILE}"
+        echo "starting task sync: $(date --iso-8601=seconds)"
+        task-pull >> "${TASK_SYNC_LOG_FILE}" 2>&1
+        task-push >> "${TASK_SYNC_LOG_FILE}" 2>&1
+        echo "task sync running"
+        unalias rm
+        rm -f "${TASK_SYNC_LOCK_FILE}"
+    fi
 }
 
-task-sync
+task-sync &
